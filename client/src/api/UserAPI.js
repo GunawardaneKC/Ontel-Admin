5d82173fd4fe0edfe9501e7fe77a00c8f5c5281c
Devin Sandanayake <92626638+devsandanayake@users.noreply.github.com>
2024-01-16 19:46:29 +0530

    update

--- client/src/api/UserAPI.js
+++ client/src/api/UserAPI.js
@@ -13,7 +13,7 @@ function UserAPI(token) {
         if(token){
             const getUser = async () =>{
                 try {
-                    const res = await axios.get('/user/infor', {
+                    const res = await axios.get('/user/infor', { withCredentials: true }, {
                         headers: {Authorization: token}
                     })
 
