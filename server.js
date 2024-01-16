783f48cac7326e59baef9d81e27ff5c767e7513f
Devin Sandanayake <92626638+devsandanayake@users.noreply.github.com>
2024-01-16 19:39:43 +0530

    uo

--- server.js
+++ server.js
@@ -11,13 +11,6 @@ const DB_URL = process.env.MONGODB_URL;
 
 const app = express();
 
-// Enable CORS middleware for each set of routes
-app.use('/user', cors(), require('./routes/userRouter'));
-app.use('/api', cors(), require('./routes/categoryRouter'));
-app.use('/api', cors(), require('./routes/upload'));
-app.use('/api', cors(), require('./routes/productRouter'));
-app.use('/api', cors(), require('./routes/paymentRouter'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
@@ -25,6 +18,15 @@ app.use(cookieParser());
 app.use(fileUpload({
     useTempFiles: true
 }));
+
+// Enable CORS middleware for each set of routes
+app.use('/user', cors(), require('./routes/userRouter'));
+app.use('/api', cors(), require('./routes/categoryRouter'));
+app.use('/api', cors(), require('./routes/upload'));
+app.use('/api', cors(), require('./routes/productRouter'));
+app.use('/api', cors(), require('./routes/paymentRouter'));
+ 
+
 
 // Connect to MongoDB with additional options
  
