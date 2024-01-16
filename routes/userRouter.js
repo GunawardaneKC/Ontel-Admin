ef6c5e028a30d2a5bc05bad11353fff60a336301
Devin Sandanayake <92626638+devsandanayake@users.noreply.github.com>
2024-01-16 21:06:25 +0530

    up

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,7 +6,7 @@ router.post('/register', userCtrl.register)
 
 router.post('/login', userCtrl.login)
 
-router.get('/logout', userCtrl.logout)
+// router.get('/logout', userCtrl.logout)
 
 router.post('/refresh_token', userCtrl.refreshToken)
 
