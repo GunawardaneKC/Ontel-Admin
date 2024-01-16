74e91f03548a0937d2d3da6798b733e93a8e3bf3
Devin Sandanayake <92626638+devsandanayake@users.noreply.github.com>
2024-01-16 19:23:25 +0530

    up

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 const DB_URL = process.env.MONGODB_URL;
 
 const app = express();
