331fa20408e369898bac4f774aee2b7c950a7bcd
Devin Sandanayake <92626638+devsandanayake@users.noreply.github.com>
2024-01-16 19:21:21 +0530

    update

--- server.js
+++ server.js
@@ -1,78 +1,43 @@
-require('dotenv').config()
+require('dotenv').config();
 const bodyParser = require('body-parser');
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const fileUpload = require('express-fileupload')
-const cookieParser = require('cookie-parser')
-const path = require('path')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
+const DB_URL = process.env.MONGODB_URL;
 
+const app = express();
 
-const app = express()
+// Enable CORS middleware for each set of routes
+app.use('/user', cors(), require('./routes/userRouter'));
+app.use('/api', cors(), require('./routes/categoryRouter'));
+app.use('/api', cors(), require('./routes/upload'));
+app.use('/api', cors(), require('./routes/productRouter'));
+app.use('/api', cors(), require('./routes/paymentRouter'));
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-// app.use(express.static('upload'));
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-    origin: '*',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
-  
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
 app.use(fileUpload({
     useTempFiles: true
-}))
+}));
 
-// Routes
-app.use('/user', require('./routes/userRouter'))
-app.use('/api', require('./routes/categoryRouter'))
-app.use('/api', require('./routes/upload'))
-app.use('/api', require('./routes/productRouter'))
-app.use('/api', require('./routes/paymentRouter'))
-// const postEmp = require('./routes/emp');
-// const postRepair = require('./routes/postRepair');
-// const postOrd = require('./routes/paymentRouter');
-// const postPro = require('./routes/productRouter');
-// const postDelivery = require('./routes/postDelivery');
-// const postWarranty = require('./routes/postWarranty');
-
-
-//routes middelware
-// app.use(postEmp);
-// app.use(postRepair);
-// app.use(postOrd);
-// app.use(postDelivery);
-// app.use(postWarranty);
-// app.use(postPro);
-
-// app.get("/",(req,res)=>{
-//     res.send("upload file")
-// })
-
-// Connect to mongodb
-// const DB_URL='mongodb+srv://happyitpvindya:happy123@itp.ftafmbd.mongodb.net/'
-const DB_URL = process.env.MONGODB_URL
-mongoose.set('strictQuery', false);
-mongoose.set('strictQuery', true);
-
-mongoose.connect(DB_URL)
-.then(()=>{
+// Connect to MongoDB with additional options
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+.then(() => {
     console.log('DB Connected');
 })
-.catch((err)=> console.log('DB connection error',err));
+.catch((err) => console.log('DB connection error', err));
 
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static('client/build'))
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-//     })
-// }
-
-
-app.listen(PORT, () =>{
-    console.log('Server is running on port', PORT)
-})
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT);
+});
